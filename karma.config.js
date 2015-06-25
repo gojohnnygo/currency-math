a4a938e1ebaded9fabfd8467e7615c23dda84928
Johnny Nguyen <hellojohnnguyen@gmail.com>
2015-06-24 20:36:07 -0700

    (feat) Add testing

---
+++ karma.config.js
@@ -0,0 +1,27 @@
+module.exports = function(config) {
+  config.set({
+
+    basePath: '',
+    frameworks: ['browserify', 'jasmine'],
+
+    files: [
+        'src/**/*.js',
+        'test/**/*.js'
+    ],
+
+    exclude: [
+    ],
+
+    preprocessors: {
+        'src/**/*.js': ['browserify'],
+        'test/**/*.js': ['browserify']
+    },
+
+    browserify: {
+        debug: true,
+        transform: [ 'babelify' ]
+    },
+
+    // define reporters, port, logLevel, browsers etc.
+  });
+};
---
+++ test/Currency-spec.js
@@ -0,0 +1,90 @@
+import {Calculator} from './calculator';
+
+describe('Calculator', () => {
+
+   it('should add two numbers', () => {
+    console.log('holler')
+
+       let calculator = new Calculator();
+
+       let sum = calculator.add(1,4);
+
+       expect(sum).toBe(5);
+   });
+
+    it('should subtract two numbers', () => {
+
+        let calculator = new Calculator();
+
+        let sum = calculator.subtract(4,1);
+
+        expect(sum).toBe(3);
+    });
+
+});
+
+// describe('Instatiation', () => {
+//   it('expects instatiation', () => {  
+//     var currency = new Currency();
+//     expect(currency.constructor.name).toEqual('Currency');
+//   }  
+
+//   it('expects instatiation with a number', () => {  
+//     var currency = new Currency(4);
+//     expect(currency).toEqual('$4.00');
+//   }
+
+//   it('expects instatiation with string', () => {  
+//     var currency = new Currency('$4.00');
+//     expect(currency).toEqual('$4.00');
+//   }
+// });
+
+// describe('Addition', () => {
+//   it('expects number arguments to be added to currency value', () => {  
+//     var currency = new Currency();
+//     currency.sum([10.95, 11.00, 3.25]);
+//     currency.sum(1);
+//     expect(num).toEqual('$26.20');
+//   });
+
+//   it('expects string arguments to be added to currency value', () => {
+//     var currency = new Currency();
+//     currency.sum(['$10.95', '$11.00', '$3.25']);
+//     currency.sum('$1.00');
+//     expect(currency).toEqual('$26.20');
+//   });
+// });
+
+// describe('Multiplication', () => {
+//   it('expects number arguments to be multiplied by currency value', () => {
+//     var currency = new Currency(2);
+//     currency.multiply([3, 3]);
+//     currency.multiply(3)
+//     expect(product).toEqual('$54.00');
+//   });
+
+//   it('expects string values multiplied by multipliers array', () => {
+//     var product = currency.multiply("$4.00", true, '50%', '25%');
+//     expect(product).toEqual('$7.50');
+//   });
+// });
+
+// describe('Division', () => {
+//   it('expects string value divided by divisor', () => {
+//     var product = currency.divide("$10.00", 2);
+//     expect(product).toEqual(['$5.00', '$5.00']);
+//   });  
+
+//   it('expects string value divided by divisor', () => {
+//     var product = currency.divide("$10.00", 3);
+//     expect(product).toEqual(['$3.34', '$3.33', '$3.33']);
+//   });  
+// });
+
+// describe('Percent Of', () => {
+//   it('expects percet of value and result', () => {
+//     var product = currency.percentOf("$10.00", "$5.00");
+//     expect(product).toEqual('50%');
+//   });
+// });
